29a028ca7f4671f368da222572b751733b584e99
Черняков Алексей Валерьевич <a.chernyakov@echelon.lan>
2025-02-27 16:11:21 +0300

    Working algrithm for HW1

---
+++ Homework1/geneticalgorithm.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+const (
+	tournamentSize = 3
+	popSize        = 100
+	generations    = 100
+	crossProb      = 0.7
+	mutProb        = 0.1
+	xMin           = 2.0
+	xMax           = 4.0
+)
+
+func randChoice[T any](arr []T) T {
+	return arr[rand.Intn(len(arr))]
+}
+
+func fitness(x float64) float64 {
+	numerator := math.Cos(math.Exp(x))
+	denominator := math.Sin(math.Log(x))
+	return numerator / denominator
+}
+
+func genIndividual() float64 {
+	return xMin + (xMax-xMin)*rand.Float64()
+}
+
+func genPopulation() (res []float64) {
+	res = make([]float64, popSize)
+
+	for i := range res {
+		res[i] = genIndividual()
+	}
+	return
+}
+
+func tournamentSelection(population []float64) (best float64) {
+	best = randChoice(population)
+	for i := 1; i < tournamentSize; i++ {
+		contender := randChoice(population)
+		if fitness(contender) > fitness(best) {
+			best = contender
+		}
+	}
+	return
+}
+
+// Одноточечный кроссинговер
+func crossingover(p1, p2 float64) float64 {
+	if rand.Float64() < crossProb {
+		return (p1 + p2) / 2
+	}
+	return p1
+}
+
+// Пока такой, можно написать лучше
+func mutate(ind float64) float64 {
+	if rand.Float64() < mutProb {
+		delta := (rand.Float64() - 0.5) * 0.1
+		mutant := ind + delta
+		if mutant < xMin {
+			mutant = xMin
+		} else if mutant > xMax {
+			mutant = xMax
+		}
+		return mutant
+	}
+	return ind
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	population := genPopulation()
+
+	bestIndividual := population[0]
+	maxExtremum := fitness(bestIndividual)
+
+	for gen := 0; gen < generations; gen++ {
+		newPopulation := genPopulation()
+
+		for i := range newPopulation {
+			p1 := tournamentSelection(population)
+			p2 := tournamentSelection(population)
+			child := crossingover(p1, p2)
+			child = mutate(child)
+			newPopulation[i] = child
+		}
+		population = newPopulation
+
+		for _, ind := range population {
+			value := fitness(ind)
+			if value > maxExtremum {
+				bestIndividual = ind
+				maxExtremum = value
+			}
+		}
+		fmt.Printf("Поколение %d: x = %.10f; f(x) = %.10f\n", gen, bestIndividual, maxExtremum)
+	}
+	fmt.Printf("Лучшее решение, найденное алгоритмом: f(%.10f) = %.10f\n", bestIndividual, maxExtremum)
+}
